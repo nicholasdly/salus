b39dd11a1d0b139a6e74b60441ce14057b9d968c
Nicholas Ly <nichdly@gmail.com>
2022-12-17 23:00:33 -0600

    Completely new implementation based off of Golang redux

--- README.md
+++ README.md
@@ -1,54 +1,9 @@
-# Security Identifier Search (SIS)
-### Financial security identifier data simplifed into a Python search engine.
-![SIS Start Page](images/sise-start.png)
-![SIS Search Results](images/sise-search.png)
-
-_This was my submission to my first hackathon, ShellHacks 2022, which I attended virtually!_
-
-## Inspiration
-
-After carefully reading through the various sponsor challenges at ShellHacks 2022, Schonfeld's
-"Street ID Challenge" had caught my attention! I knew Python (a language I am more than
-comfortable with) was brilliant for data analysis, but I've never actually done realistic
-data analysis with Python and its libraries. I've never tried to make a search engine of any
-kind, and with very little experience in Pandas or NumPy, attacking Schonfeld's challenge felt
-like a great opprotunity to learn more about Python!
-
-## What Is It?
-
-The Security Identifier Search (or SIS)
-is a local search engine that returns the most relevant financial securities found in a given
-database based off a user search term or query.
-
-## How I Built It
-
-Being developed in Python, I used **pandas** and **numpy** for the database analysis,
-**difflib** to calculate relevancy, **time** to calculate search performance, and **tkinter**
-for the user interface!
-
-## Challenges I Ran Into
-
-After quick research on pandas and numpy, I immediately knew they would be faster than anything
-I could code from scratch. I was able to create a working terminal-based version of the search
-engine in an hour or two, but that is where my first problem arose. The first issue I ran into
-was time performance: calculating similarity between two strings is slow! My first version of
-the search engine took well over two minutes to return any results, and much of the weekend was
-actually spent researching and playing with the code to improve its performance.
-
-The second challenge I ran into was how to design this search engine in a way that would be
-enjoyable to use. Although it worked well inside of a terminal, no general customer would want
-to be fiddling with a terminal! The issue being I've never made a user interface in Python that
-wasn't based in the terminal! Thorough research on my options led to tkinter and its
-documentation, which led to the current user interface.
-
-## Accomplishments That I'm Proud Of
-
-I am proud of what I've learned throughout the process, including data analysis fundamentals,
-string similarity metrics, and user interface deisgn! I went from knowing nothing to being
-comfortable in all three topics, and the finished project is something I would happily show to
-anyone.
-
-## What's Next?
-
-Despite my accomplishments, I can easily recognize my searching and sorting algorithm is quite
-slow, and it is something I plan on continuously researching to improve!
+<p align="center">
+  <img src="assets/logo.png" width="100"></img>
+</p>
+<h3 align="center">Salus is a financial security identifier search engine.</h3>
+<p align="center">
+  Built in Python and powered by C and C++, <b>Salus</b> is capable of reading, indexing, and sorting hundreds of thousands of CSV records in a matter of seconds!
+  <br/>
+  What started as my very first submission to a hackathon has grown to be a fun learning experience on the basics of search engine mechanics and data science.
+</p>
--- main.py
+++ main.py
@@ -1,270 +1,68 @@
 
-import tkinter as tk
-import tkinter.ttk as ttk
-from numpy import vectorize
-from pandas import DataFrame, read_csv
-from difflib import SequenceMatcher
-from time import perf_counter
+# Used for CSV file processing and data representation; along with pyarrow,
+# pandas can read a CSV file much faster than Python's built in csv module.
+import pandas as pd
 
-# NOTE: Using numpy/pandas to calculate longest common substring might be faster
-# than using difflib; will need to do some testing. Might also consider
-# calculating similarity based off of edit distance, but results would be
-# dramatically different.
+# Used for longest common substring computation; pylcs is powered by C++ and
+# therefore much faster than any individually written script or built-in
+# Python module.
+import pylcs
 
-PATH_SECURITIES = "assets/securities.csv"
-PATH_PRIORITIES = "assets/priorities.txt"
-MAX_RESULTS = 5
-
-class SISE:
+class Salus:
     """
-    Represents the Security Identifier Search Engine.
+    Represents the Salus financial security identifier search engine.
     """
 
-    def __init__(self):
+    def __init__(self) -> None:
         """
-        Initializes security data and user interface.
+        Initializes the Salus financial security identifier search engine.
         """
-        print("Reading input data...")
-        with open(PATH_SECURITIES) as database:
-            next(database)
-            data = (line for line in database)
-            self.search_strings = DataFrame(data, columns=["line"])
+        self._data = None  # The financial security dataframe
 
-        print("Establishing a bit of logic...")
-        self.data = read_csv(PATH_SECURITIES)  # Dataframe of original data
-        self.sm = SequenceMatcher()
-        self.lcs_vectorized = vectorize(self.get_lcs)
-        self.results = None  # List of most relevant security indices
-
-        print("Making user interface...")
-        self.window = tk.Tk()
-        self.window.title("SISE Search")
-        self.window.resizable(width=False, height=False)
-
-        self.window.columnconfigure(0, weight=1)
-        self.window.rowconfigure(0, weight=1)
-        self.window.rowconfigure(1, weight=4)
-        self.window.rowconfigure(2, weight=1)
-
-        self.search_frame = self.create_search_frame()
-        self.search_frame.grid(column=0, row=0, padx=5, pady=3)
-
-        self.results_frame = self.create_results_frame()
-        self.results_frame.grid(column=0, row=1, padx=5, sticky="EW")
-
-        self.tip_frame = self.create_tip_frame()
-        self.tip_frame.grid(column=0, row=2, padx=5, pady=5)
-
-        self.window.mainloop()
-
-    ############################################################################
-    # WINDOW RELATED FUNCTIONS
-    ############################################################################
-
-    def create_search_frame(self):
+    def read(self, filename: str) -> None:
         """
-        Creates the frame where the search bar exists.
+        Reads a given CSV file using pandas and pyarrow, creating the
+        financial security dataframe, and generates a simplified vector
+        space model for each financial security.
+
+        filename -- string: Filepath of CSV
         """
-        frame = ttk.Frame(master=self.window)
-
-        # Establishes frame layout
-        frame.columnconfigure(0, weight=1)
-        frame.columnconfigure(1, weight=3)
-        frame.columnconfigure(2, weight=1)
-        frame.rowconfigure(0, weight=1)
-
-        # Creates search label
-        lbl_search = ttk.Label(frame, text="Search:")
-        lbl_search.grid(column=0, row=0, padx=3)
-
-        # Creates search bar
-        ent_search = ttk.Entry(frame)
-        ent_search.focus()
-        ent_search.grid(column=1, row=0)
-
-        # Creates search button
-        btn_search = ttk.Button(
-            frame,
-            text="Search",
-            command=lambda: self.on_search(ent_search.get().lower())
+        self._data = pd.read_csv(
+            filename,
+            keep_default_na=False,
+            engine='pyarrow'
         )
-        btn_search.grid(column=2, row=0, padx=5, pady=5)
-        ent_search.bind(
-            "<Return>",
-            lambda _: self.on_search(ent_search.get().lower())
-        )
+        self._data['vsm'] = self._data.astype(str).values.sum(axis=1)
 
-        return frame
-
-    def create_results_frame(self):
+    def query(self, term: str, results: int = 1) -> list[pd.Series]:
         """
-        Creates the frame where the search results exist.
+        Queries the financial security dataframe for the most relevant
+        financial securities based off of longest common substring to a
+        specified search term.
+
+        term -- string: Search term
+        results -- int (default=1): Maximum length of list of query results 
+
+        returns -> list: List of most relevant financial securities as pandas series
         """
-        frame = ttk.Frame(master=self.window, relief=tk.RAISED, borderwidth=1)
-
-        # Establishes frame layout
-        frame.columnconfigure(0, weight=1)
-        frame.rowconfigure(0, weight=1)
-
-        # Creates search results list box
-        lbx_results = tk.Listbox(frame, height=MAX_RESULTS, selectmode="SINGLE")
-        lbx_results.grid(column=0, row=0, sticky="EW")
-        lbx_results.bind("<Double-1>", lambda _: self.on_select(lbx_results))
-
-        return frame
-
-    def create_tip_frame(self):
-        """
-        Creates the frame where the tip text exists.
-        """
-        frame = ttk.Frame(master=self.window)
-
-        # Establishes frame layout
-        frame.columnconfigure(0, weight=1)
-        frame.rowconfigure(0, weight=1)
-
-        # Creates search results list box
-        lbl_tip = ttk.Label(frame, text="Made by Nicholas Ly!")
-        lbl_tip.grid(column=0, row=0)
-
-        return frame
-
-    ############################################################################
-    # EVENT RELATED FUNCTIONS
-    ############################################################################
-
-    def on_search(self, query):
-        """
-        Submits search query.
-        """
-        lbl_tip = self.tip_frame.winfo_children()[0]
-        lbl_tip.config(text="Loading, please wait...")
-        lbl_tip.update()
-
-        lbx_results = self.results_frame.winfo_children()[0]
-        print("Fetching relevant results...")
-
-        if query:
-            self.set_relevancy_values(query)
-            self.results = self.get_top_results()
-            print(self.results)
-
-            # Displays search results in listbox
-            lbx_results.delete(0, "end")
-            for i, result in enumerate(self.results):
-                lbx_results.insert(i, self.get_relevant_id(int(result)))
-
-            lbl_tip.config(
-                text="Search complete!"
-            )
-
-        else:
-            print("Empty query!")
-
-            lbl_tip.config(
-                text="Error! Empty search attempt!"
-            )
-
-    def on_select(self, lbx):
-        """
-        Opens properties menu.
-        """
-        window = tk.Toplevel()
-
-        # Establishes window properties and layout
-        window.title("Properties")
-        window.resizable(width=False, height=False)
-        window.columnconfigure(0, weight=1)
-        window.rowconfigure(0, weight=1)
-
-        frame = ttk.Frame(master=window)
-
-        # Establishes frame layout
-        frame.columnconfigure(0, weight=1)
-        frame.columnconfigure(0, weight=2)
-        frame.rowconfigure(0, weight=1)
-
-        label_text = ("Security ID:", "CUSIP:", "SEDOL:", "ISIN:", "RIC:",
-                      "Bloomberg:", "BBG:", "Symbol:", "Root Symbol:",
-                      "BB Yellow:", "SPN:")
-
-        # Creates each street ID name label
-        for i, text in enumerate(label_text):
-            label = ttk.Label(frame, text=text)
-            label.grid(column=0, row=i, padx=5, pady=2)
-
-        # Gets the street IDs of the selected security
-        index = self.results[lbx.curselection()[0]]
-        street_ids = self.data.iloc[index].values
-
-        # Creates the labels in which the actual street IDs are displayed
-        for i, id in enumerate(street_ids):
-            label = ttk.Label(frame, text=id)
-            label.grid(column=1, row=i, padx=5, pady=2)
-
-        frame.grid(column=0, row=0, padx=25, pady=10)
-
-    ############################################################################
-    # LOGIC RELATED FUNCTIONS
-    ############################################################################
-
-    def get_lcs(self, col):
-        """
-        Returns the length of the longest common substring between the query and
-        a second string.
-        """
-        self.sm.set_seq2(str(col).lower())
-        return self.sm.find_longest_match()[2]
-
-    def set_relevancy_values(self, query):
-        """
-        Adds relevancy data to dataframe based off of longest common substring
-        (LCS).
-        """
-        start = perf_counter()
-        self.sm.set_seq1(query)
-
-        self.search_strings["LCS"] = self.lcs_vectorized(
-            self.search_strings["line"]
-        )
-        self.search_strings = self.search_strings.sort_values(
-            "LCS", ascending=False
-        )
+        # Must have read a CSV file before querying
+        if self._data is None:
+            raise AttributeError('Salus must read a CSV file before querying!')
         
-        stop = perf_counter()
-        print("Query term {} took {} seconds to sort {} entries."\
-            .format(repr(query), round(stop-start, 3), len(self.data))
-        )
-
-    def get_top_results(self):
-        """
-        Returns the security IDs of the securities with the longest common
-        substring to the query.
-        """
-        results = self.search_strings[
-            self.search_strings["LCS"] == self.search_strings["LCS"].max()
-        ]
-        return list(results.index.values)[:MAX_RESULTS]
-        
-    def get_relevant_id(self, index):
-        """
-        Given a security's index, return its most relevant street ID.
-        """
-        security = list(self.data.iloc[index])
-        similarity_data = []
-
-        for id in security:
-            if isinstance(id, str):
-                self.sm.set_seq2(str(id).lower())
-                similarity_data.append(self.sm.quick_ratio())
-            else:
-                similarity_data.append(0)
-
-        return security[similarity_data.index(max(similarity_data))]
-
+        indices = []
+        for index, vsm in enumerate(self._data['vsm']):
+            score = pylcs.lcs_string_length(term, vsm)
+            indices.append( (score, index) )
+        indices.sort()
+        return [ self._data.iloc[indices[-i-1][1]] for i in range(min(results, len(indices))) ]
 
 def main():
-    sise = SISE()
+    salus = Salus()
+    salus.read('data/securities.csv')
+    results = salus.query('FJUN')
+    
+    for series in results:
+        print(series.values)
 
 if __name__ == "__main__":
     main()
